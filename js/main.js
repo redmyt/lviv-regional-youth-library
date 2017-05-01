fd02b1f98f4c68fb88c4f98a906cb6da4da4e075
dmytro-revak <dmytro27@gmail.com>
2017-05-01 15:45:18 +0300

    remove unnecessary errors in style file

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@
     switchTimeStyles('.navigation__collapsed-button', 'navigation__collapsed-button_style_night');
     switchTimeStyles('.main-content', 'main-content_style_night');
     switchTimeStyles('.main-content-article', 'main-content-article_style_night');
-    switchTimeStyles('.news-board__more-articles-button', 'news-board__more-articles-button_stlye_night');
+    switchTimeStyles('.news-board__more-articles-button', 'news-board__more-articles-button_style_night');
 
     // Get Ukraine time from any point of word
     function getUkraineTime() {
