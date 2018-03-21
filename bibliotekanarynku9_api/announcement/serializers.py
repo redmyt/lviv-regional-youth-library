7571b48988a4836241409602ace8d384919e7252
dmytrorevak <dmytro.revak@gmail.com>
2018-03-21 22:50:08 +0200

    Announcement app
    Create the appropriate file sturcure.
    Add Annoucement and AnnouncementTranslation models.
    Add serilizers.py file where describe AnnounceSerializer and AnnouncementTranslationSerializer.
    Create migrations file.

---
+++ bibliotekanarynku9_api/announcement/serializers.py
@@ -0,0 +1,58 @@
+"""
+Module that describes the serializers for the Announcement and
+AnnouncementTranslation models
+"""
+
+from rest_framework import serializers
+from announcement.models import Announcement, AnnouncementTranslation
+
+
+class AnnouncementSerializer(serializers.ModelSerializer):
+    """Announcement model serializer"""
+
+    class Meta:
+        model = Announcement
+        fields = '__all__'
+
+
+class AnnouncementTranslationSerializer(serializers.ModelSerializer):
+    """AnnouncementTranslationSerializer model serializer"""
+
+    announcement = AnnouncementSerializer()
+
+    class Meta:
+        model = AnnouncementTranslation
+        fields = '__all__'
+
+    def create(self, validated_data):
+        announcement_data = validated_data.pop('announcement')
+        announcement = Announcement.objects.create(**announcement_data)
+        announcement_translation = AnnouncementTranslation.objects.create(
+            announcement=announcement,
+            **validated_data)
+        return announcement_translation
+
+    def update(self, instance, validated_data):
+        announcement_data = validated_data.pop('announcement')
+        announcement = instance.announcement
+
+        instance.language = validated_data.get('language', instance.language)
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get(
+            'description',
+            instance.description)
+        instance.organizer = validated_data.get(
+            'organizer',
+            instance.organizer)
+        instance.save()
+
+        if isinstance(announcement_data, dict):
+            announcement.avatar = announcement_data.get(
+                'avatar',
+                announcement.avatar)
+            announcement.start_at = announcement_data.get(
+                'start_at',
+                announcement.start_at)
+            announcement.save()
+
+        return instance
