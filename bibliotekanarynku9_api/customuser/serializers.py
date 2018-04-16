fc52a9ee1fa205ecb1bbfd662ff21f1035db2048
dmytrorevak <dmytro.revak@gmail.com>
2018-04-16 23:57:30 +0300

    CustomUser app
    Refactor authentication app by adding the User app.
    Add User app to the settings file.
    Update the migrations files.

--- bibliotekanarynku9_api/customuser/serializers.py
+++ bibliotekanarynku9_api/customuser/serializers.py
@@ -3,12 +3,12 @@ Module that describes the serializer for the User model.
 """
 
 from rest_framework import serializers
-from authentication.models import User
+from customuser.models import CustomUser
 
 
 class UserSerializer(serializers.ModelSerializer):
     """UserSerializer model serializer"""
 
     class Meta:
-        model = User
+        model = CustomUser
         fields = ('first_name', 'last_name', 'email')
