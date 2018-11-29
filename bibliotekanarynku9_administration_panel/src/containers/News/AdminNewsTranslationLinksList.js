7c1fcf1a37b8262d093950a9912602b8cacaa6cf
dmytrorevak <dmytro.revak@gmail.com>
2018-11-29 15:20:04 +0200

    Admin NewsPost Section
    Created the NewsPost administration block.
    Created the appropriate services, components and containers using the structure
    of Admin Announcement section.
    Fixed the newspost link creation on the backend.
    Updated the `AdminRouter` to render the `AdminNews` container and
    `AdminNewsItem` component.

---
+++ bibliotekanarynku9_administration_panel/src/containers/News/AdminNewsTranslationLinksList.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import AdminLinkField from '../../components/AdminLinkField/AdminLinkField';
+
+const headLabelStyle = {
+    fontWeight: 'bold'
+};
+
+class AdminNewsTranslationLinksList extends React.Component {
+
+    handleLinkUpdateClick = (linkId, label, href) => {
+        this.props.onLinkUpdateClick(linkId, label, href);
+    }
+
+    handleLinkRemoveClick = linkId => {
+        this.props.onLinkRemoveClick(linkId);
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    !!this.props.links.length && (
+                        <Typography component='p' variant='subheading'>
+                            <span style={headLabelStyle}>{'Links:'}</span>
+                        </Typography>
+                    )
+                }
+                {
+                    this.props.links.map(link => (
+                        link && (
+                            <AdminLinkField
+                                key={link.id}
+                                id={link.id}
+                                label={link.label}
+                                href={link.href}
+                                isEdit={this.props.isEdit}
+                                isError={this.props.isError}
+                                onUpdateClick={this.handleLinkUpdateClick}
+                                onRemoveClick={this.handleLinkRemoveClick}
+                            />
+                        )
+                    ))
+                }
+            </div>
+        );
+    }
+}
+
+export default AdminNewsTranslationLinksList;
---
+++ bibliotekanarynku9_administration_panel/src/containers/News/adminNewsService.js
@@ -0,0 +1,110 @@
+import axios from 'axios';
+import {apiPath, getCSRFToken, removeBase64Prefix, LANGUAGE_CODES} from '../../helpers';
+
+const newsPath = 'news_post/';
+
+export const getNewsListService = (inputUrl) => {
+    const url = inputUrl || apiPath + newsPath;
+    return axios.get(url);
+};
+
+export const getNewsById = (newsId) => {
+    const url = `${apiPath}${newsPath}${newsId}/`;
+    return axios.get(url);
+};
+
+export const postNewsService = avatar => {
+    const url = `${apiPath}${newsPath}`;
+    let data = {avatar: removeBase64Prefix(avatar)};
+    return axios.post(url, data, {
+        headers: {
+            'X-CSRFToken': getCSRFToken()
+        }
+    });
+};
+
+export const postNewsTranslationService = (newsId, title, description, language) => {
+    const url = `${apiPath}${newsPath}${newsId}/translation/`;
+    return axios.post(url, {
+        title: title,
+        description: description,
+        language: LANGUAGE_CODES[language]
+    }, {
+        headers: {
+            'X-CSRFToken': getCSRFToken()
+        }
+    });
+};
+
+export const postNewsTranslationLinkService = (newsId, translationId, label, href) => {
+    const url = `${apiPath}${newsPath}${newsId}/translation/${translationId}/link/`;
+    return axios.post(url, {
+        label: label,
+        href: href
+    }, {
+        headers: {
+            'X-CSRFToken': getCSRFToken()
+        }
+    });
+};
+
+export const putNewsService = (newsId, avatar) => {
+    const url = `${apiPath}${newsPath}${newsId}/`;
+    let data = {avatar: removeBase64Prefix(avatar)};
+    return axios.put(url, data, {
+        headers: {
+            'X-CSRFToken': getCSRFToken()
+        }
+    });
+};
+
+export const putNewsTranslationService = (newsId, translationId, title, description) => {
+    const url = `${apiPath}${newsPath}${newsId}/translation/${translationId}/`;
+    return axios.put(url, {
+        title: title,
+        description: description
+    }, {
+        headers: {
+            'X-CSRFToken': getCSRFToken()
+        }
+    });
+};
+
+export const putNewsTranslationLinkService = (newsId, translationId, linkId, label, href) => {
+    const url = `${apiPath}${newsPath}${newsId}/translation/${translationId}/link/${linkId}/`;
+    return axios.put(url, {
+        label: label,
+        href: href
+    }, {
+        headers: {
+            'X-CSRFToken': getCSRFToken()
+        }
+    });
+};
+
+export const deleteNewsService = newsId => {
+    const url = `${apiPath}${newsPath}${newsId}/`;
+    return axios.delete(url, {
+        headers: {
+            'X-CSRFToken': getCSRFToken()
+        }
+    });
+};
+
+export const deleteNewsTranslationService = (newsId, translationId) => {
+    const url = `${apiPath}${newsPath}${newsId}/translation/${translationId}/`;
+    return axios.delete(url, {
+        headers: {
+            'X-CSRFToken': getCSRFToken()
+        }
+    });
+};
+
+export const deleteNewsTranslationLinkService = (newsId, translationId, linkId) => {
+    const url = `${apiPath}${newsPath}${newsId}/translation/${translationId}/link/${linkId}/`;
+    return axios.delete(url, {
+        headers: {
+            'X-CSRFToken': getCSRFToken()
+        }
+    });
+};
