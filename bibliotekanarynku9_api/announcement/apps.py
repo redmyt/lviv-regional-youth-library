3d0e74b900442520e25b40bceb457a94f16d82a8
Dmytro Revak <dmytro.revak@gmail.com>
2020-05-12 17:58:24 +0300

    Google My Business Announcement Translation create signal (#150)
    Created the signals and receivers modules that contains the main signals
    initiation code.
    Add the send signal method to the AnnouncementTranslation model.
    Send signal at the POST view.

--- bibliotekanarynku9_api/announcement/apps.py
+++ bibliotekanarynku9_api/announcement/apps.py
@@ -2,4 +2,19 @@ from django.apps import AppConfig
 
 
 class AnnouncementConfig(AppConfig):
-    name = 'announcement'
+    name = "announcement"
+
+    def ready(self):
+        """
+        Register the Django signals that are related to the Announcement application.
+        """
+
+        from announcement.models import AnnouncementTranslation
+        from announcement.signals import ANNOUNCEMENT_TRANSLATION_CREATED
+        from announcement.receivers import synchronize_announcement_translation
+
+        ANNOUNCEMENT_TRANSLATION_CREATED.connect(
+            synchronize_announcement_translation,
+            sender=AnnouncementTranslation,
+            dispatch_uid="ANNOUNCEMENT_TRANSLATION_CREATED",
+        )
