0193242ad08d5815e334387fc518da371bb7ba38
Dmytro Revak <dmytro.revak@gmail.com>
2020-09-16 15:15:55 +0300

    Optimize header scrolling behavior
    Added logic that checks do we need to apply the "fixed" syles to navbar.
    Removed some unneeded method calls.

--- bibliotekanarynku9_ui/src/js/header.js
+++ bibliotekanarynku9_ui/src/js/header.js
@@ -1,28 +1,40 @@
+var handleNavigationScroll = getNavigationScrollHandler();
+
 // Add scroll event handler for window and make navigation fixed when user scroll window over the navigation block
 $(window).on('scroll', function () {
-    var navigationHeight = getHeaderItemsParameters().navigationHeight,
-        windowTopScroll = getHeaderItemsParameters().windowTopScroll,
-        headerHeight = getHeaderItemsParameters().headerHeight;
-
-    // When scroll position is over header height add fixed styles for navigation block
-    if (windowTopScroll > headerHeight - navigationHeight) {
-        $($navigation).addClass('header__navigation_fixed');
-        $($header).css('padding-bottom', navigationHeight);
-    } else {
-        $($navigation).removeClass('header__navigation_fixed');
-        $($header).css('padding-bottom', 0);
-    }
+    handleNavigationScroll();
 
     // Collapse menu when user fire scroll event
-    if (!$navItemsList.hasClass('navigation__items-list_style_collapsed')) {
-        $navItemsList.addClass('navigation__items-list_style_collapsed');
-    }
+    $navItemsList.addClass('navigation__items-list_style_collapsed');
 });
 
 // Show or hide collapsed menu when user click the collapsed button
 $collapsedButton.on('click', function () {
     $navItemsList.toggleClass('navigation__items-list_style_collapsed');
 });
+
+// Get method that has its own closure state and implements sticky navigation logic.
+function getNavigationScrollHandler() {
+    var isNavigationFixed = false;
+
+    return function() {
+        var headerItemsParameters = getHeaderItemsParameters(),
+            navigationHeight = headerItemsParameters.navigationHeight,
+            windowTopScroll = headerItemsParameters.windowTopScroll,
+            headerHeight = headerItemsParameters.headerHeight;
+
+        // When scroll position is over header height add fixed styles for navigation block
+        if (windowTopScroll > headerHeight - navigationHeight && !isNavigationFixed) {
+            $($navigation).addClass('header__navigation_fixed');
+            $($header).css('padding-bottom', navigationHeight);
+            isNavigationFixed = true;
+        } else if (windowTopScroll < headerHeight - navigationHeight && isNavigationFixed) {
+            $($navigation).removeClass('header__navigation_fixed');
+            $($header).css('padding-bottom', 0);
+            isNavigationFixed = false;
+        }
+    };
+}
 
 // Get header-block-element and navigation-block-element heights an save them. Also get current window scroll position.
 function getHeaderItemsParameters() {
